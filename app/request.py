12e8f65e2784ad3cdfdb1ade243f0cc848a2eed9
Guibrandao01 <you@example.com>
2021-08-20 10:09:37 -0300

    automatizando

--- app/request.py
+++ app/request.py
@@ -1,63 +1,116 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
-response = requests.get('https://www.infomoney.com.br/cotacoes/petrobras-petr4/historico/',headers={'User-Agent': 'Mozilla/5.0'})
-#https://economia.uol.com.br/cotacoes/bolsas/acoes/bvsp-bovespa/petr4-sa/
-print(response.status_code)
-print(response.headers)
-print(response.content)
+import time
 
+v = 0
+v1 = 0
+c2 = 0
 
-content = response.content
-#especificando que o conteudo da pagina esta em html
-site  = BeautifulSoup(content,'html.parser')
-print(site.prettify())
+v3 = str(input('\n digite o valor ideal para compra '))
+v3 = str(v3)
 
+v4 = str(input('\n digite o valor ideal para venda'))
+v4 = str(v4)
 
+def job():    
+    response = requests.get('https://www.infomoney.com.br/cotacoes/petrobras-petr4/historico/',headers={'User-Agent': 'Mozilla/5.0'})
+    #https://economia.uol.com.br/cotacoes/bolsas/acoes/bvsp-bovespa/petr4-sa/
+    # print(response.status_code)
+    # print(response.headers)
+    # print(response.content)
+       
+    content = response.content
+    #especificando que o conteudo da pagina esta em html
+    site  = BeautifulSoup(content,'html.parser')
+    # print(site.prettify())
+    noticias = site.find('div',attrs={'id':'results_box'})
+    noticias
 
+    abertura1 = site.find('div', attrs={'class': 'value'})
+    abertura1.text
+    abertura = abertura1.find('p')
+    abertura.text    
 
-noticias = site.find('div',attrs={'id':'results_box'})
-noticias
-
-
-abertura1 = site.find('div', attrs={'class': 'value'})
-abertura1.text
-abertura = abertura1.find('p')
-abertura.text    
-
-
-minimo1 = site.find('div', attrs={'class': 'minimo'})
-minimo1.text
-minimo = minimo1.find('p')
-minimo.text    
-
-
-
-maxima1 = site.find('div',attrs={'class': 'maximo'})
-maxima1.text
-maxima = maxima1.find('p')
-maxima.text
-
-lista = []
-
-if(noticias):
-    lista.append([abertura.text ,minimo.text ,maxima.text])
-else:
-    lista.append(['','',''])
-
+    atual = abertura.text
+    v1 = atual
+   
+  
     
-lista        
+    
+    minimo1 = site.find('div', attrs={'class': 'minimo'})
+    minimo1.text
+    minimo = minimo1.find('p')
+    minimo.text
+    minn = minimo.text
+    v = minn
+    
+    
+    maxima1 = site.find('div',attrs={'class': 'maximo'})
+    maxima1.text
+    maxima = maxima1.find('p')
+    maxima.text
+    maxx = maxima.text
+    v2 = maxx
+    
+    # variacao1 = site.find('td',attrs={'class': 'positive'})
+    # variacao1.text
+    lista = []
+    
+    lista.append([abertura.text ,minimo.text ,maxima.text])
+    
+    lista        
+            
+    df = pd.DataFrame(lista, columns=['Abertura','Minima','Maxima'])
+    # print(df)
+
+      #salvando df com os dados do dia
+    tste = df.to_csv('dadosDia0208.csv', index=False)
+    
+    print('\n valor atual em ', atual)
+    time.sleep(2)
+    print("\n A minima chegou em",minn)
+    time.sleep(2)
+    print("\n A maxima chegou em",maxx)
+    time.sleep(2)
         
-df = pd.DataFrame(lista, columns=['Abertura','Minima','Maxima'])
+    
+    
+    if atual == v3:
+        print('\n valor atual chegou no valor informado para compra', atual)
+        time.sleep(2)
+    if atual == v4:
+        print('\n valor atual chegou no falor informado para venda', atual)
+        
+        
+        
+   
+        
+         
+job()
 
 
-print(df)
-#salvando df com os dados do dia
-tste = df.to_csv('dadosDia2107.csv', index=False)
 
 
-dff = pd.read_csv('dadosDia.csv')
-dff2 = pd.read_csv('noticias1.csv')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     
 
 
--- app/treinamento.py
+++ app/treinamento.py
@@ -1,26 +1,59 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+# import pickle
+from request import job
 
-df = pd.read_csv('1507e1607.csv',decimal=',')
-dff = pd.read_csv('dadosDia2107.csv',decimal=',')
-
-fechamento = df['FECHAMENTO']
-features = df[['ABERTURA','MÁXIMO','MÍNIMO']]
-
-dff
-treino = features
-
-#x_teste e y_teste sao as classes
-
-X_treino,X_teste,y_treino,y_teste=train_test_split(treino,fechamento,
-                                                   random_state=42)
-
-equacao=LinearRegression()
-
-
-equacao.fit(X_treino,y_treino)
+import schedule
+import time
 
 
 
-previsao = equacao.predict(dff)
+
+def treinamentos():
+    df = pd.read_csv('2017a2021.csv',decimal=',')
+    dff = pd.read_csv('dadosDia0208.csv',decimal=',')
+    
+    fechamento = df['FECHAMENTO']
+    features = df[['ABERTURA','MÍNIMO','MÁXIMO']]
+    
+    features
+    treino = features
+    
+    X_treino,X_teste,y_treino,y_teste=train_test_split(treino,fechamento,
+                                                       random_state=42)
+    equacao=LinearRegression()
+
+    treinamento = equacao.fit(X_treino,y_treino)
+    
+    previsao = equacao.predict(dff)
+    
+    string = str(previsao)
+    
+    print('\n A previsao atualizada do fechamento da ação é ',string)
+    time.sleep(2)
+    
+                                #### SALVANDO PREVISOES ####
+                                
+    # with open('previsoes.txt','a') as file:
+    #     file.write('\n')
+    #     file.write('DIA 0308')
+    #     file.write(string)
+    
+    
+                                ##### SALVANDO MODELO #####
+                                
+    # with open('modelo.pkl', 'wb') as file:
+    #     pickle.dump(treinamento, file)
+schedule.every(5).seconds.do(job)
+schedule.every(5).seconds.do(treinamentos)
+       
+treinamentos()
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+
